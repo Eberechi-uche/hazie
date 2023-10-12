e1f0f1ae1da0eb4df476d7d862b0a509a1bb8895
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-10-12 08:59:54 +0100

    add ability to rearrange items in collections page

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -2,13 +2,16 @@
 
 import ImageCard from "@/components/ui/card/ImageCard";
 import { CollectionContext } from "@/contexts/collectionCtx";
-import { Flex, Text, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Text, Heading, SimpleGrid, Box } from "@chakra-ui/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Fragment, useContext, useState } from "react";
+import Draggable, { DraggableCore } from "react-draggable";
 
 export default function Collection() {
   const { currentCollection } = useContext(CollectionContext);
   const [images, setImageArray] = useState(currentCollection.collectionItem);
+  const [] = useState();
   const route = useRouter();
   let dragElement: string;
   let targetElement: string;
@@ -32,9 +35,11 @@ export default function Collection() {
     newArray.splice(dragElementPosition, 1, targetItem);
     setImageArray(newArray);
   }
-
+  function handleDragStart(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault;
+  }
   return (
-    <Flex flexDir={"column"} w={"100%"}>
+    <Flex flexDir={"column"} w={"100%"} minH={"100vh"}>
       <Text
         fontSize={"xs"}
         my={"8"}
@@ -45,7 +50,7 @@ export default function Collection() {
       >
         &#8592; Back to Collection
       </Text>
-      <Flex w={"100%"} justify={"space-between"}>
+      <Flex w={"100%"} justify={"space-between"} my={"4"}>
         <Flex flexDir={"column"}>
           <Heading fontSize={"lg"} textTransform={"uppercase"}>
             {currentCollection.name}
@@ -55,24 +60,35 @@ export default function Collection() {
         <Text fontSize={"xs"}> Edit Image position</Text>
       </Flex>
 
-      <SimpleGrid
-        columns={[2, 3, 4]}
-        gap={"2"}
-        my={"12"}
+      <Flex
+        p={"8"}
         transition={"all 0.5s ease-in-out"}
+        position={"relative"}
+        flex={1}
+        gap={"4"}
+        h={"100%"}
+        w={"100%"}
+        flexWrap={"wrap"}
+        bg={"brand.offwhite"}
+        borderRadius={"6px"}
+        justify={"center"}
       >
         {currentCollection.collectionItem.length > 0 &&
           images.map((item) => (
-            <Fragment key={item.id}>
-              <ImageCard
-                {...item}
-                getDragElementRef={getDragElementRef}
-                getTargetElementRef={UpdatePositon}
-                handleDragEnd={handleDragEnd}
+            <Draggable bounds={"parent"}>
+              <Box
+                className="handle drag"
+                bgImage={`url(${item.imageSm})`}
+                onDragStart={handleDragStart}
+                w={"100px"}
+                h={"120px"}
+                bgSize={"cover"}
+                borderRadius={"6px"}
+                m={"2"}
               />
-            </Fragment>
+            </Draggable>
           ))}
-      </SimpleGrid>
+      </Flex>
     </Flex>
   );
 }
